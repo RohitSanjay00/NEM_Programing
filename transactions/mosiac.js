798ad7f652d055469826047cc505f7336b52cf4f
RohitSanjay00 <51485506+RohitSanjay00@users.noreply.github.com>
2020-06-29 11:29:53 +0400

    Update mosiac.js

--- transactions/mosiac.js
+++ transactions/mosiac.js
@@ -1,18 +1,18 @@
 let nem = require("nem-sdk").default;
 let endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
 
-let common = nem.model.objects.create("common")("SolarGridX","d14f9fe9d02b2e61a2a80ae443191ac9473cf6514cb44d7527075dca2e30a906");
+let common = nem.model.objects.create("common")("Password","Private Key");
 
-let transferTransaction = nem.model.objects.create("transferTransaction")("TC4O7TF3KPCYXKPOYB6VHLMOJJFRFWI5PWLPETAH", 1, "Sent mosaic");
+let transferTransaction = nem.model.objects.create("transferTransaction")("Senders Address", 1, "Message for the sender");
 
 var mosaicDefinitions = nem.model.objects.get("mosaicDefinitionMetaDataPair");
 
-var mosaicAttachment = nem.model.objects.create("mosaicAttachment")("ps1", "ps1_tokens", 100);
+var mosaicAttachment = nem.model.objects.create("mosaicAttachment")("NameSpace", "Token Name", amount of token wanting to send);
 
 transferTransaction.mosaics.push(mosaicAttachment);
 
 nem.com.requests.namespace.mosaicDefinitions(endpoint, mosaicAttachment.mosaicId.namespaceId).then(function(res){
-  var definition = nem.utils.helpers.searchMosaicDefinitionArray(res.data, ["ps1_tokens"]);
+  var definition = nem.utils.helpers.searchMosaicDefinitionArray(res.data, ["Token Name"]);
   var fullName = nem.utils.format.mosaicIdToName(mosaicAttachment.mosaicId);
   mosaicDefinitions[fullName] = {};
   mosaicDefinitions[fullName].mosaicDefinition = definition[fullName];
