2869c368e997d025a71b72a9a119c8dad429ba88
RohitSanjay00 <51485506+RohitSanjay00@users.noreply.github.com>
2020-06-29 11:30:19 +0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let nem  = require("nem-sdk").default;
-
-let endpoint  = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
-
-nem.com.requests.chain.lastBlock(endpoint).then(function(res){
-  console.log(res);
-},function(err){
-  console.log(err);
-})
